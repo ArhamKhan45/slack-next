a7b749aff75184c243e6ee1464ff1baa300533c2
ArhamKhan45 <karham45@gmail.com>
2024-11-09 17:00:00 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="text-rose-500 font-bold">
+      Home
+      <Button variant={"slack"}>click me</Button>
+    </div>
+  );
+};
+
+export default Home;
